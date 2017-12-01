69933f9f96dc3d98cb864bc97f4152569bdbc9d9
Benoît Claveau <benoit.claveau@izzili.com>
2017-12-01 10:30:56 +0100

    stream

--- tests/qwebs-auth-jwt.spec.js
+++ tests/qwebs-auth-jwt.spec.js
@@ -28,13 +28,13 @@ describe("auth", () => {
             name: "My Name",
             version: 3
         }
-        expect(auth.encode(payload)).to.be("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiTXkgTmFtZSIsInZlcnNpb24iOjN9.m69S2NJymL3HA08_PWvsJ07WPtjtyPfXCon9A5ckd7E");
+        expect(auth.encodeSync(payload)).to.be("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiTXkgTmFtZSIsInZlcnNpb24iOjN9.m69S2NJymL3HA08_PWvsJ07WPtjtyPfXCon9A5ckd7E");
     });
 
     it("decode", () => {
         const auth = new QwebsAuthJwt(config);
         const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiTXkgTmFtZSIsInZlcnNpb24iOjN9.m69S2NJymL3HA08_PWvsJ07WPtjtyPfXCon9A5ckd7E";
-        const payload = auth.decode(token);
+        const payload = auth.decodeSync(token);
         expect(payload.name).to.be("My Name");
         expect(payload.version).to.be(3);
     });
@@ -51,7 +51,8 @@ describe("auth", () => {
             expect(error.statusCode).to.be(401)
         }
         const res2 = await client.post({ url: "http://localhost:3000/connect", json: { id: 1024 }});
-
-        const res3 = await client.get({ url: "http://localhost:3000/info", auth: { "bearer": res2.body }, json: true });
+        expect(res2.body.token).not.to.be(null);
+        const res3 = await client.get({ url: "http://localhost:3000/info", auth: { "bearer": res2.body.token }, json: true });
+        expect(res3.body).to.eql({ message: "I'm authorized."});
     });
 });
--- tests/services/info.js
+++ tests/services/info.js
@@ -5,27 +5,21 @@
 */
 "use strict"
 
-const through2 = require("through2");
-
 class InfoService {
 	constructor($auth) {
         this.auth = $auth;
 	};
     
     async connect(ask, reply) {
-        //read ask body;
-        //const token = await this.auth.encode({ id: 1024});
-        //reply.end({ token });
-        ask.pipe(through2.obj(async (chunk, enc, callback) => {
-            const token = await this.auth.encode(chunk);
-            callback(null, {token});
-        })).pipe(reply)
+        reply.outputType = "object";
+        ask.pipe(this.auth.encode()).pipe(reply);
     };
     
 	async getInfo(ask, reply) {
-        await this.auth.identify(ask, reply);
-        const message = "I'm authorized."
-		reply.end({ message });
+        // await this.auth.identify(ask, reply);
+        // const message = "I'm authorized."
+        // reply.end({ message });
+        ask.pipe(this.auth.identify()).pipe(reply)
 	};
 };
 
